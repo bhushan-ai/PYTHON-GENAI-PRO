4029291654699bdbcecd260f53252fe1db282055
bhushan-ai <bhushaningole2004@gmail.com>
2026-09-14 14:46:39 +0530

    Add examples for looping through lists and using enumerate for menu items

---
+++ 04_Loops/03_loop_through_list.py
@@ -0,0 +1,4 @@
+orders = ["Bhushan", "carlos", "nia", "peter"]
+
+for name in orders:
+    print(f"Order ready for {name}")
---
+++ 04_Loops/04_enumerate.py
@@ -0,0 +1,4 @@
+menu  = ["lemon", "ginger", "mint", "green"]
+
+for idx, item in enumerate(menu, start= 1):
+    print(f"{idx} : {item} chai")
