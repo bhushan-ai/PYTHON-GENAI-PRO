2e99ef41f999bb028255d048474a19b056f5900f
bhushan-ai <bhushaningole2004@gmail.com>
2026-09-16 13:42:28 +0530

    Add initial function implementations for order printing, sales data fetching, bill calculation, and VAT addition

---
+++ 05_Functions/01_duplication.py
@@ -0,0 +1,5 @@
+def print_order(name, order):
+    print(f'{name} order {order} chai!')
+
+print_order("bhushan","masala")
+print_order("carlos","lemon")
---
+++ 05_Functions/02_complex.py
@@ -0,0 +1,16 @@
+def fetch_sales():
+    print("fetching sales data")
+
+def filter_valid_data():
+    print("Filtering valid sales data")
+
+def summarizing_data():
+    print("Summarizing sales data")
+
+def generate_report():
+    fetch_sales()
+    filter_valid_data()
+    summarizing_data()
+    print("Report is ready")
+
+generate_report()
